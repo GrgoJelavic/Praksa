f590d15b5a5439fd815b3ac014d5a7a937354f94
Grgo Jelavic <9r9o@My-Mac.local>
2020-10-05 20:14:58 +0200

    10-JS-basics_import_npm

--- js/index.js
+++ js/index.js
@@ -1,88 +1,37 @@
-function onSidebarToggle() {
-    const sidebarRef = document.querySelector("#sidebar-container");
-    sidebarRef.classList.toggle("sidebar--open");
-}
+ import { createBox } from "./utils.js";
+ import { DIV_RED_BOX } from "./html.constants.js"
+ import * as moment from "./node_modules/moment/dist/moment.js"
+ import { DIV_RED_BOX } from "./html.constants.js";
 
-const boxes = document.querySelectorAll(".box"); // boxes je lista html elemenata
 
-boxes.forEach((box) => box.addEventListener("click", onBoxClick));
+ function onSidebarToggle() {
+     const sidebarRef = document.querySelector("#sidebar-container");
+     sidebarRef.classList.toggle("sidebar--open");
+ }
 
-// forEach, map, filter, reduce
-const students = [
-    { id: 0, name: "Matea", gender: "female", age: 21 },
-    { id: 1, name: "Grgo", gender: "male", age: 23 },
-    { id: 2, name: "Ivan", gender: "male", age: 30 },
-];
+ const boxes = document.querySelectorAll(".box"); // boxes je lista html elemenata
 
-// ############# MAP ################
-// *** For petlja ***
-// const names = [];
-// for (let i = 0; i < students.length; i++) {
-//   const student = students[i];
-//   names.push(student.name);
-// }
+ boxes.forEach((box) => box.addEventListener("click", onBoxClick));
 
-// *** foreach ***
-// const names = [];
-// students.forEach((student) => names.push(student.name));
+ function onBoxClick(event) {
+     const boxRef = event.currentTarget; // u boxRef je spremljen: document.getElementById("blue-box")
 
-// *** map ***
-const names = students.map((student) => student.name);
+     onDuplicateBox(boxRef.id);
+ }
 
-console.log(names);
+ function onDuplicateBox(id) {
+     const elementRef = document.getElementById(id);
+     const newBox = elementRef.cloneNode();
 
-// ############# FILTER ################
-// *** For petlja ***
-// const males = [];
-// for (let i = 0; i < students.length; i++) {
-//   const student = students[i];
-//   if (student.gender === "male") {
-//     males.push(student.name);
-//   }
-// }
+     newBox.addEventListener("click", onBoxClick);
+     document.getElementById("box-container").append(newBox);
+ }
 
-// *** filter ***
-const males = students.filter((student) => student.gender === "male");
-// const males = students
-//   .filter((student) => student.gender === "male")
-//   .map((male) => male.name);
+ function onSubmit(event) {
+     event.preventDefault(); // Izbjeći automatsko refreshanje stranice
 
-console.log(males);
+     const emailValue = document.getElementById("mail").value;
+     alert(emailValue);
+ }
 
-// ############# REDUCE ################
-// *** For petlja ***
-// let totalAge = 0;
-// for (let i = 0; i < students.length; i++) {
-//   const student = students[i];
-//   totalAge += student.age; // Isto kao: totalAge = totalAge + student.age;
-// }
-
-// *** reduce ***
-const totalAge = students.reduce((total, student) => (total += student.age), 0);
-console.log(totalAge);
-
-// for (let i = 0; i < boxes.length; i++) {
-//   const box = boxes[i];
-//   box.addEventListener("click", onBoxClick);
-// }
-
-function onBoxClick(event) {
-    const boxRef = event.currentTarget; // u boxRef je spremljen: document.getElementById("blue-box")
-
-    onDuplicateBox(boxRef.id);
-}
-
-function onDuplicateBox(id) {
-    const elementRef = document.getElementById(id);
-    const newBox = elementRef.cloneNode();
-
-    newBox.addEventListener("click", onBoxClick);
-    document.getElementById("box-container").append(newBox);
-}
-
-function onSubmit(event) {
-    event.preventDefault(); // Izbjeći automatsko refreshanje stranice
-
-    const emailValue = document.getElementById("mail").value;
-    alert(emailValue);
-}
+ boxes.forEach(box => box.addEventListener("click", onBoxClick));
